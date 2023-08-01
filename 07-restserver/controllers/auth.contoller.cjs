4a7e9f563ed0c08859df71c1d81abb002db24dff
Anghelo Alva <ance.anghelo@gmail.com>
2023-07-31 20:21:00 -0400

    Google Authentication agregado, y creacion de ruta + validacion del google token

--- 07-restserver/controllers/auth.contoller.cjs
+++ 07-restserver/controllers/auth.contoller.cjs
@@ -1,7 +1,9 @@
+/* eslint-disable camelcase */
 const { response } = require('express')
 const User = require('../models/user.cjs')
 const bcryptjs = require('bcryptjs')
 const { generateJWT } = require('../helpers/generateJWT.cjs')
+const { GoogleVerify } = require('../helpers/google-verify.cjs')
 
 const login = async (req, res = response) => {
   const { email, password } = req.body
@@ -45,6 +47,26 @@ const login = async (req, res = response) => {
   }
 }
 
+const googleSingIn = async (req, res = response) => {
+  const { id_token } = req.body
+
+  try {
+    const { name, picture, email } = await GoogleVerify(id_token)
+    console.log(name, picture, email)
+
+    res.json({
+      msg: 'Google SingIn',
+      id_token
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({
+      msg: 'Google token is not valid'
+    })
+  }
+}
+
 module.exports = {
-  login
+  login,
+  googleSingIn
 }
