4e09644d87bddc0659d7140f9ab735d164359208
Anghelo Alva <ance.anghelo@gmail.com>
2023-08-02 20:34:53 -0400

    nuevo helper para la creacion del nombre y ruta del archivo subido

--- 07-restserver/controllers/upload.controller.cjs
+++ 07-restserver/controllers/upload.controller.cjs
@@ -1,8 +1,7 @@
 const { request, response } = require('express')
-const { v4: uuidv4 } = require('uuid')
-const path = require('path')
+const { uploadArchive } = require('../helpers/upload-archive.cjs')
 
-const loadArchive = (req = request, res = response) => {
+const loadArchive = async (req = request, res = response) => {
   if (!req.files || Object.keys(req.files).length === 0) {
     return res.status(400).send({
       msg: 'No files were uploaded.'
@@ -15,28 +14,10 @@ const loadArchive = (req = request, res = response) => {
     })
   }
 
-  const { archive } = req.files
+  const name = await uploadArchive(req.files)
 
-  const newName = archive.name.split('.')
-  const extension = newName[newName.length - 1]
-
-  const extensions = ['png', 'jpg', 'jpeg', 'gif']
-
-  if (!extensions.includes(extension)) {
-    return res.status(400).send({
-      msg: 'Invalid extension. Valid extensions: ' + extensions.join(', ')
-    })
-  }
-
-  const tempName = uuidv4() + '.' + extension
-  const uploadPath = path.join(__dirname, '../uploads/', tempName)
-
-  archive.mv(uploadPath, (err) => {
-    if (err) {
-      return res.status(500).json({ err })
-    }
-
-    res.send('File uploaded tos' + uploadPath)
+  res.json({
+    name
   })
 }
 
---
+++ 07-restserver/helpers/upload-archive.cjs
@@ -0,0 +1,30 @@
+const { v4: uuidv4 } = require('uuid')
+const path = require('path')
+
+const uploadArchive = async (files, validExtensions = ['png', 'jpg', 'jpeg', 'gif'], folder = '') => {
+  return new Promise((resolve, reject) => {
+    const { archive } = files
+    const newName = archive.name.split('.')
+    const extension = newName[newName.length - 1]
+
+    if (!validExtensions.includes(extension)) {
+      // eslint-disable-next-line prefer-promise-reject-errors
+      return reject(`Invalid extension. Valid extensions: ${validExtensions.join(', ')}`)
+    }
+
+    const tempName = uuidv4() + '.' + extension
+    const uploadPath = path.join(__dirname, '../uploads/', folder, tempName)
+
+    archive.mv(uploadPath, (err) => {
+      if (err) {
+        return reject(err)
+      }
+
+      resolve(tempName)
+    })
+  })
+}
+
+module.exports = {
+  uploadArchive
+}
